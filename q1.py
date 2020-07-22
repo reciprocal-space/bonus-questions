6c4a4b3e3fe87618ac164d1007ebf97aa6facd25
reciprocal-space <vivekbgohil@gmail.com>
2020-07-21 21:35:52 -0400

    add response to q1

---
+++ q1.py
@@ -0,0 +1,36 @@
+import sys
+
+def romanToArabic(input):
+    """
+        Function to map a roman number to arabic number
+    """
+    numberMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+    
+    sum = 0
+    for c in input:
+        try:
+            sum += numberMap[c]
+        except:
+            print('Unrecognized character \'{0}\' - please unsure all characters are Roman and try again'.format(c))
+            return False
+    print('Arabic representation of number is {0}!'.format(sum))
+    return sum
+
+if __name__ == "__main__":
+    """
+       Convert roman to arabic numerals
+    """
+    if len(sys.argv) > 1:
+        print('Converting arguments from stdin to arabic numerals!')
+        for i in range(1, len(sys.argv)):
+            romanToArabic(sys.argv[i])
+    else:
+        print('Please provide commandline arguments or import the function to use in your own code!')
