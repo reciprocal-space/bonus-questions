eec44943889df815a8601527d3e5085d6773b38b
reciprocal-space <vivekbgohil@gmail.com>
2020-08-03 21:36:24 -0400

    fix minor bugs

--- q2.py
+++ q2.py
@@ -16,10 +16,7 @@ x1H1 + x2H2 =< Y
 P(x1, x2) = x1P1 + x2P2 (Maximize)
 """
 
-# tests = (X, Y, P1, P2, H1, H2)
-t1 = (240, 320, 40, 30, 2, 1)
-t2 = (300, 380, 70, 45, 3, 1)
-t3 = (180, 420, 65, 55, 3, 2)
+import sys
 
 def calculateProfit(acres,profitPerAcre):
     x1 = acres[0]
@@ -31,46 +28,45 @@ def calculateProfit(acres,profitPerAcre):
 def findMaxTurningPoint(input):
     """
     """
-        X = input[0]
-        Y = input[1]
-        P1 = input[2]
-        P2 = input[3]
-        H1 = input[4]
-        H2 = input[5]
+    X = input[0]
+    Y = input[1]
+    P1 = input[2]
+    P2 = input[3]
+    H1 = input[4]
+    H2 = input[5]
 
-        inflectionPoints = [(0,0),(x1, 0),(0, x2),(x1*H1,0),(0,x2*H2),((Y-X*H2)/(H1-H2),(X-(Y-X*H2)/(H1-H2))))]
-        
-        maxProfit = 0
-        x1 = 0
-        x2 = 0
-        for point in inflectionPoints:
-            profit = calculateProfit(point, (P1,P2))
-            if profit > maxProfit:
-                x1 = point[0]
-                x2 = point[1]
-                maxProfit = profit
+    maxProfit = 0
+    x1 = 0
+    x2 = 0
+    inflectionPoints = [(0,0),(x1, 0),(0, x2),(x1*H1,0),(0,x2*H2),((Y-X*H2)/(H1-H2),(X-(Y-X*H2)/(H1-H2)))]
+    for point in inflectionPoints:
+        profit = calculateProfit(point, (P1,P2))
+        if profit > maxProfit:
+            x1 = point[0]
+            x2 = point[1]
+            maxProfit = profit
 
-            else:
-                pass
+        else:
+            pass
 
-        print('A maximum estimated profit of ${0} would be given by planting {1} and {2} acres of corn and oats respectively'.format(maxProfit, x1, x2))
-        return 
+    print('A maximum estimated profit of ${0} would be given by planting {1} and {2} acres of corn and oats respectively'.format(maxProfit, x1, x2))
+    return 
 
 
 if __name__ == '__main__':
     """
     """
     if len(sys.argv) > 1:
-        if sys.argv == 'test':
+        if sys.argv[1] == 'test':
             t1 = (240, 320, 40, 30, 2, 1)
             t2 = (300, 380, 70, 45, 3, 1)
             t3 = (180, 420, 65, 55, 3, 2)
 
-            for t, index in enumerat([t1,t2,t3]):
-                print('Running Test {0}'.format(index))
+            for index, t in enumerate([t1,t2,t3]):
+                print('Running Test {0}'.format(index + 1))
                 findMaxTurningPoint(t)
         
-        else if type(sys.argv[1]) == tuple:
+        elif type(sys.argv[1]) == tuple:
             if len(tuple) == 6:
                 print('Estimating optimum land distribution for maximising profit from corn and oats!')
             else:
@@ -80,6 +76,6 @@ if __name__ == '__main__':
             print('Error: please provide a tuple of the form (X, Y, P1, P2, H1, H2)')
     else:
         print('Please supply a commandline arguement - \'test\' to run tests or a tuple of the form (X, Y, P1, P2, H1, H2) to calculate a new optimization')
-    return
+
     
 
